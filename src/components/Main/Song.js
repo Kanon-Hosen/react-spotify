28b62b7b726196fd624fffebabd20220bc43c31e
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-09-27 16:08:33 +0600

    top chart added

--- src/components/Main/Song.js
+++ src/components/Main/Song.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import './Song.css'
 const Song = (props) => {
+    const { title, subtitle, share } = props.song;
     return (
-        <div>
-            <h1>{props.song.title}</h1>
+        <div className='song'>
+            <div className="img">
+            <img src={share.image} alt={title} />
+            </div>
+            <p>{title.slice(0,15)}...</p>
+            <h6>{subtitle.slice(0,20)}...</h6>
         </div>
     );
 };
